1b6f65d78c0b55c20c09fe8efca6162393c638fa
Darren Britton <Darren Britton>
2017-02-06 03:34:01 +0000

    updated api url to ssl

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API_KEY = '287abb69cc8fc0f96fd6baeebfffcede';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH-WEATHER';
 
